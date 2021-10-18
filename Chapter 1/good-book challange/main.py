dc97e375cae6b43967892af76ac9ce7d1f546393
S-Alikhonov <saidalikhonalikhonov@gmail.com>
2021-10-18 12:00:23 +0200

    chapter 2

--- Chapter 1/good-book challange/main.py
+++ Chapter 1/good-book challange/main.py
@@ -1,7 +1,7 @@
 from data_framer import *
 
 
-url_book_list = 'https://www.goodreads.com/list/show/11.Best_Crime_Mystery_Books'
+url_book_list = 'https://www.goodreads.com/list/show/348.Thrillers'
 how_many = 500 
 
 file_name = dataframer(url_book_list,how_many)
