6326841c43861639590dee124d1ef00c6ac629a8
S-Alikhonov <saidalikhonalikhonov@gmail.com>
2022-01-18 12:07:23 +0100

    q3 and q4 added

--- Chapter4/SQL/d2.sql
+++ Chapter4/SQL/d2.sql
@@ -9,7 +9,7 @@ SELECT
 	FROM film
 	
 	INNER JOIN film_category USING(film_id)
-	INNER JOIN category USING(category_id)
+	INNER JOIN category USING(category_id);
 
 -- Select customer and their address join country and city tables
 -- Name column names with table name by using snake case.
@@ -31,15 +31,24 @@ INNER JOIN  city USING(city_id)
 INNER JOIN country USING(country_id);
  
 
-
-
-
-
 -- Question 3 :
 -- Select all payments info including staff info, customer info, and rental info.
 -- Name column names with table name by using snake case.
 
-
+SELECT 
+	p.amount payment_amount,
+	p.payment_date,
+	s.staff_id,
+	s.first_name || ' ' || s.last_name staff_full_name,
+	c.first_name || ' ' || c.last_name customer_full_name,
+	r.rental_date,
+	r.return_date
+FROM payment p
+INNER JOIN staff s 
+	ON (p.staff_id = s.staff_id)
+INNER JOIN customer c
+	ON (p.customer_id = c.customer_id)
+INNER JOIN rental r USING(rental_id);
  
 
  
@@ -49,8 +58,12 @@ INNER JOIN country USING(country_id);
 -- Name column names with table name by using snake case.
 -- HINT : You need to use groupby
 
-
-
+SELECT
+ 	a.first_name || ' ' ||  a.last_name actor_full_name,
+	f.title film_title
+FROM actor a
+INNER JOIN film_actor USING(actor_id)
+INNER JOIN film f USING(film_id)
  
 
 -- Question 5 :
