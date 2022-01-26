156982f3ad02e973e0fa180eb3c68542968be0cc
S-Alikhonov <saidalikhonalikhonov@gmail.com>
2022-01-26 20:37:39 +0100

    iris solved

--- Chapter4/pyspark/D13. Spark/script.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.feature import StringIndexer
-from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
-spark = SparkSession.builder.appName('ml-iris').getOrCreate()
